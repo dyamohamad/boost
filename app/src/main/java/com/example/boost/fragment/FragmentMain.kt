c20c8c889106dec972311bf81ab24fb2fbb73099
dyamohamad <nurdiyanamohamad@gmail.com>
2019-12-05 17:14:42 +0800

    Add singleLiveEvent class,work on databinding

--- app/src/main/java/com/example/boost/fragment/FragmentMain.kt
+++ app/src/main/java/com/example/boost/fragment/FragmentMain.kt
@@ -1,18 +1,21 @@
 package com.example.boost.fragment
 
-import androidx.lifecycle.ViewModelProviders
+import android.app.AlertDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.boost.R
 import com.example.boost.adapter.ListAdapter
 import com.example.boost.databinding.FragmentMainFragmentBinding
 import com.example.boost.model.TopicDetails
 import com.example.boost.viewmodel.FragmentMainViewModel
+import kotlinx.android.synthetic.main.field.view.*
 
 class FragmentMain : Fragment() {
 
@@ -39,6 +42,10 @@ class FragmentMain : Fragment() {
         binding.viewmodel = viewModel
         binding.lifecycleOwner = this
 
+        viewModel.onClickAdd.observe(viewLifecycleOwner, Observer {
+            showAlertDialogToAddNewTopic()
+        })
+
         initRecyclerView()
     }
 
@@ -50,7 +57,35 @@ class FragmentMain : Fragment() {
         binding.recyclerView.adapter = adapter
         binding.recyclerView.setHasFixedSize(true)
     }
+    fun showAlertDialogToAddNewTopic() {
+        val builder = AlertDialog.Builder(getContext())
+        val view = layoutInflater.inflate(R.layout.field, null)
+        builder.setView(view)
 
+        // set up the ok button
+        builder.setPositiveButton(android.R.string.ok) { dialog, p1 ->
+            val newCategory = view.et_new_topic.text.toString()
+            var isValid = true
+            if (newCategory!!.isBlank()) {
+                isValid = false
+            }
+
+            if (isValid) {
+
+
+            }
+
+            if (isValid) {
+                dialog.dismiss()
+            }
+        }
+
+        builder.setNegativeButton(android.R.string.cancel) { dialog, p1 ->
+            dialog.cancel()
+        }
+
+        builder.show()
+    }
 
     private val list = arrayListOf(
         TopicDetails(resources.getString(R.string.first_title), 0,10),
--- app/src/main/java/com/example/boost/viewmodel/FragmentMainViewModel.kt
+++ app/src/main/java/com/example/boost/viewmodel/FragmentMainViewModel.kt
@@ -1,11 +1,20 @@
 package com.example.boost.viewmodel
 
+import android.view.View
 import androidx.lifecycle.ViewModel
 import com.example.boost.R
 import com.example.boost.model.TopicDetails
+import com.example.boost.util.SingleLiveEvent
 
 class FragmentMainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+
+    val onClickAdd: SingleLiveEvent<View> = SingleLiveEvent()
+
+    fun onClickAddNewTopic(v: View) {
+        onClickAdd.postValue(v)
+    }
+
 
 
 }
